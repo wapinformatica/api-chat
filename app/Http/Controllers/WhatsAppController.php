e7d35169cf55e507882a688c646763a8772a0129
luis-felipe-senarmt-dev <luis.amorim@senarmt.org.br>
2023-09-22 09:52:21 -0400

    Correção

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -24,16 +24,36 @@ class WhatsAppController extends Controller
                 'type' => 'return'
             ]);
 
-            $sslOptions = array(
-                "ssl"=>array(
-                    "verify_peer"=>false,
-                    "verify_peer_name"=>false,
+            $url = 'https://hsgee.senarmt.org.br/api/whats-app';
+            $curl = curl_init();
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'message' => 'GRAVADO COM SUCESSO', // Envia o texto
                 ),
-            );
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json'
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            // $sslOptions = array(
+            //     "ssl"=>array(
+            //         "verify_peer"=>false,
+            //         "verify_peer_name"=>false,
+            //     ),
+            // );
 
-            Http::post('https://hsgee.senarmt.org.br/api/whats-app', file_get_contents('', false, stream_context_create($sslOptions)), [
-                'message' => $request
-            ]);
+            // Http::post('https://hsgee.senarmt.org.br/api/whats-app', file_get_contents('', false, stream_context_create($sslOptions)), [
+            //     'message' => $request
+            // ]);
 
         } catch (Exception $ex) {
             RetornWhat::create([
